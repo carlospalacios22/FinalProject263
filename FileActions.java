5bcee560b99137b724cb46a819cde3f98a6a44ba
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-04-15 13:33:46 -0700

    Create FileActions.java

---
+++ FileActions.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class FileActions {
+    private static final String LOG_FILE = "log.txt";
+
+    public static void saveLog(String logEntry) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+            writer.write(logEntry + System.lineSeparator());
+        }
+    }
+
+    public static ArrayList<String> readLog() throws IOException {
+        ArrayList<String> logEntries = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(LOG_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                logEntries.add(line);
+            }
+        }
+        return logEntries;
+    }
+
+    public static String createLogEntry(String message) {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return dateTime.format(formatter) + " - " + message;
+    }
+
+
+public static void deleteLog() throws IOException {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, false))) {
+        writer.write("");
+    }
+}
+
+}
