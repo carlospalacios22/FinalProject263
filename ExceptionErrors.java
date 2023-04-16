954e414264b2e7bb3506c7029925f4a7c06c2706
carlospalacios22 <122708005+carlospalacios22@users.noreply.github.com>
2023-04-16 00:33:39 -0700

    updated comments -Final

--- ExceptionErrors.java
+++ ExceptionErrors.java
@@ -1,20 +1,17 @@
-// ExceptionErrors.java
-
-
-
 import javax.swing.JOptionPane;
 
 public class ExceptionErrors {
+
+    // This method checks if the input string for the number of players is empty
+    // and throws an EmptyInputException if it is.
     public static void checkNumberOfPlayers(String input) throws EmptyInputException {
         if (input.isEmpty()) {
             throw new EmptyInputException("Number of players cannot be empty!");
         }
     }
 
+    // This method displays a message dialog with the given message and an error icon.
     public static void showMessage(String message) {
         JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
     }
 }
-
-
- 
